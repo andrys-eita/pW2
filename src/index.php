646e97a5d52e8d4ab7d23b35a7b074c77b6e7d91
andrys-eita <anndrysetec@gmail.com>
2026-03-09 10:27:27 +0000

    Final aula 01

---
+++ src/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nome = "andrys";
+    $idade = 18;
+    $salário = 3500.77;
+    $casado = false;
+    $cursos = ["php","bd","front-end"];
+
+    var_dump(
+        $nome,
+        $idade,
+        $salário,
+        $casado,
+        $cursos
+    );
+
+    // $nome,
+    // $idade,
+    // $salário,
+    // $casado,
+    // $cursos
+    //);
+
+
+
+
+    ?>
+</body>
+</html>
